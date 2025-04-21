dd08da7649d9941cd5e8bdec4c058b4dcd3f5592
githubscientist <sathish.datascientist@gmail.com>
2025-04-21 19:42:10 +0530

    nodejs server with request urls and request methods

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ const http = require('http');
 
 // create a server object
 const server = http.createServer((request, response) => {
-    console.log('Request received');
-    console.log(`Request URL: ${request.url}`);
-    console.log(`Request Method: ${request.method}`);
-    response.end('Hello World');
+    if (request.url === '/') {
+        if (request.method === 'GET') {
+            response.end('GET: Root Route');
+        } else if (request.method === 'POST') {
+            response.end('POST: Root Route');
+        } else if (request.method === 'PUT') {
+            response.end('PUT: Root Route');
+        } else if (request.method === 'DELETE') {
+            response.end('DELETE: Root Route');
+        }
+    } else if (request.url === '/test') {
+        if (request.method === 'GET') {
+            response.end('GET: Test Route');
+        } else if (request.method === 'POST') {
+            response.end('POST: Test Route');
+        } else if (request.method === 'PUT') {
+            response.end('PUT: Test Route');
+        } else if (request.method === 'DELETE') {
+            response.end('DELETE: Test Route');
+        }
+    }
 });
 
 // start the server
