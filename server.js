38c6547352dcd1941768d40a71054e334a4ae1ac
githubscientist <sathish.datascientist@gmail.com>
2025-04-22 19:31:54 +0530

    database connection done

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('./utils/config');
+
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(`Error connecting to MongoDB: ${err.message}`);
+    })
 
 // start the server
 app.listen('3001', '127.0.0.1', () => {
